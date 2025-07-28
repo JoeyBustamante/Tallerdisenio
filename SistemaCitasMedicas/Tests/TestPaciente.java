23049af7bc75454bfee69446a9b7419cd4d4ab72
Jahir124 <jahirmp13@gmail.com>
2025-07-28 15:43:55 -0500

    test file

---
+++ SistemaCitasMedicas/Tests/TestPaciente.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPaciente {
+    @org.junit.jupiter.api.Test
+    
+}
